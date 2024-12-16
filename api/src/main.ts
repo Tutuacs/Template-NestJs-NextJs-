147343b3578cb13c55f954d1faaf30e64b7e78fd
Tutuacs <arthursilva.rs1@gmail.com>
2024-12-16 18:58:57 -0300

    fix Cors

--- api/src/main.ts
+++ api/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'https://template-nest-js-next-jsfront.vercel.app',
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   });
 
