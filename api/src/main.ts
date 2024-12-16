4fcb80bed6fb80a6b1803670786e3055d5a1b488
Arthur_Silva <arthursilva.rs1@gmail.com>
2024-12-16 15:30:40 -0300

    allowedHeaders

--- api/src/main.ts
+++ api/src/main.ts
@@ -8,6 +8,7 @@ async function bootstrap() {
   app.enableCors({
     origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
   });
   app.useGlobalPipes(
     new ValidationPipe({
