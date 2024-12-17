1360b2f3fb5779f4d5b2400645809a8e7a0774c1
Arthur_Silva <arthursilva.rs1@gmail.com>
2024-12-17 10:00:29 -0300

    without allowHeaders

--- api/src/main.ts
+++ api/src/main.ts
@@ -8,7 +8,6 @@ async function bootstrap() {
   app.enableCors({
     origin: ['*','https://template-nest-js-next-jsfront.vercel.app'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
   });
 
