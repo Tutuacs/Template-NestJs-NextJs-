edbc42408d4c6aa4c5a7b36d543032da0c130658
Arthur_Silva <arthursilva.rs1@gmail.com>
2024-12-17 08:25:48 -0300

    origin * and https

--- api/src/main.ts
+++ api/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: '*',
+    origin: ['*','https://template-nest-js-next-jsfront.vercel.app'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   });
 
