435d0afbb3d5cac457edd06d1ae73d6b8e85912c
Arthur_Silva <arthursilva.rs1@gmail.com>
2024-12-17 10:18:24 -0300

    change cors

--- api/src/main.ts
+++ api/src/main.ts
@@ -3,14 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
-  app.enableCors({
-    origin: ['*','https://template-nest-js-next-jsfront.vercel.app'],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: "*",
-    credentials: true,
-  });
+  const app = await NestFactory.create(AppModule, {cors: true});
 
   app.useGlobalPipes(
     new ValidationPipe({
