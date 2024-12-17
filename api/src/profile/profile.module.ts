85e82a32c57133f722cef2f4427fae5a9df4bc1a
Arthur_Silva <arthursilva.rs1@gmail.com>
2024-12-17 10:24:30 -0300

    change vercel.json

--- api/src/profile/profile.module.ts
+++ api/src/profile/profile.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ProfileService } from './profile.service';
 import { ProfileController } from './profile.controller';
-import { PrismaModule } from 'src/prisma/prisma.module';
-import { AuthModule } from 'src/auth/auth.module';
+import { PrismaModule } from '../prisma/prisma.module';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
   imports: [PrismaModule, AuthModule],
